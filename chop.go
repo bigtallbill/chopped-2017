f1f9619861e6ecf55ed2517d0eeab8e186e8cefb
Philipp Tkachev <zoonman@gmail.com>
2017-08-29 20:47:20 -0400

    Add hash func

--- chop.go
+++ chop.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"io/ioutil"
 
-func make_hash(input string) string {
+	"io"
+	"fmt"
+)
+const HASH_LENGTH = 64
+
+func PrintLn(s string) {
+	text := []byte(s)
+	ioutil.WriteFile("/dev/stdout", text, 0777)
 
 }
 
+func RandomMd5() string {
+	b := make([]byte, HASH_LENGTH)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		PrintLn("error:")
+		PrintLn(err.Error())
+	}
+
+	h := md5.New()
+	s := string(b)
+	io.WriteString(h, s)
+	s2 := h.Sum(nil)
+
+	return fmt.Sprintf("%x", s2)
+}
+
+
+
 func main() {
-	fmt.Printf("hello")
+
+	PrintLn(RandomMd5())
 }
